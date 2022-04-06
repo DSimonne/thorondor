32d9c1e018b9eac470f635a5b34ff0f89835840d
Ravenne <51970962+DSimonne@users.noreply.github.com>
2022-04-06 21:06:10 +0100

    Update DiamondDataset class

--- thorondor/gui_iterable.py
+++ thorondor/gui_iterable.py
@@ -5,6 +5,7 @@ from datetime import datetime
 import glob
 import os
 import pickle
+import h5py
 
 import ipywidgets as widgets
 from ipywidgets import interact, Button, Layout, interactive, fixed
@@ -237,3 +238,56 @@ class Dataset():
 
         except Exception as e:
             raise e
+
+
+class DiamondDataset():
+    """
+    """
+
+    def __init__(self, filename):
+        """"""
+        self.filename = filename
+
+        with h5py.File(self.filename) as f:
+            print(self.filename)
+
+            self.group_list = list(f["entry1"]["instrument"].keys())
+            self.sequences = self.group_list[:-4]
+            print("Sequences recorded:", self.sequences)
+            for seq in self.sequences:
+                self.group = seq
+
+                # Save data
+                setattr(self, f"{seq}_binding_energy", f["entry1"]["instrument"][self.group]["binding_energy"][0, :])
+                setattr(self, f"{seq}_kinetic_energy", f["entry1"]["instrument"][self.group]["kinetic_energy"][0, :])
+                setattr(self, f"{seq}_images", f["entry1"]["instrument"][self.group]["images"][:])
+                setattr(self, f"{seq}_spectra", f["entry1"]["instrument"][self.group]["spectra"][:])
+                setattr(self, f"{seq}_spectrum", f["entry1"]["instrument"][self.group]["spectrum"][0, :])
+
+                # Save metadata
+                setattr(self, f"{seq}_count_time", f["entry1"]["instrument"][self.group]["count_time"][0])
+                setattr(self, f"{seq}_iterations", f["entry1"]["instrument"][self.group]["iterations"][0])
+                setattr(self, f"{seq}_step_energy", f["entry1"]["instrument"][self.group]["step_energy"][0])
+                setattr(self, f"{seq}_pass_energy", f["entry1"]["instrument"][self.group]["pass_energy"][0])
+                setattr(self, f"{seq}_photon_energy", f["entry1"]["instrument"][self.group]["photon_energy"][0])
+                setattr(self, f"{seq}_work_function", f["entry1"]["instrument"][self.group]["work_function"][0])
+                setattr(self, f"{seq}_y_scale", f["entry1"]["instrument"][self.group]["y_scale"][:])
+
+                try:
+                    # Create array
+                    self.arr = np.array([
+                        getattr(self, f"{seq}_binding_energy"),
+                        getattr(self, f"{seq}_kinetic_energy"),
+                        getattr(self, f"{seq}_spectrum"),
+                    ])
+
+                    # Save df
+                    df = pd.DataFrame(
+                        self.arr.T, columns=["Energy", "Mesh", "\u03BC"])
+                    setattr(self, f"{seq}_df", df)
+
+                except Exception as E:
+                    raise E
+
+    def __repr__(self):
+        return self.filename
